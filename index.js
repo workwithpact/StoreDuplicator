03b5016e45b07c47a5d8d0b3575fba35ce7bb556
Alex Leclair <alexandre.leclair@zloche.net>
2020-04-27 09:36:29 -0400

    Page migration works

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ program
   .option('--articles', 'Run the migration for articles')
   .option('--collections', 'Run the migration for collections')
   .option('--pages', 'Run the migration for pages')
+  .option('-v, --verbosity', 'Verbosity level. Defaults to 4, as talkative as my MIL.')
 
 program.parse(process.argv);
 
@@ -16,13 +17,24 @@ const start = async () => {
     shopName: process.env.SOURCE_SHOPIFY_STORE,
     apiKey: process.env.SOURCE_SHOPIFY_API_KEY,
     password: process.env.SOURCE_SHOPIFY_API_PASSWORD,
+    apiVersion: '2020-04'
   }
   const destinationStore = {
     shopName: process.env.DESTINATION_SHOPIFY_STORE,
     apiKey: process.env.DESTINATION_SHOPIFY_API_KEY,
     password: process.env.DESTINATION_SHOPIFY_API_PASSWORD,
+    apiVersion: '2020-04'
   }
-  const migration = new Migrator(sourceStore, destinationStore)
-  await migration.testConnection()
+  const migration = new Migrator(sourceStore, destinationStore, (program.verbosity && program.verbosity * 1)|| 4)
+  try {
+    await migration.testConnection()
+    console.log('Store configuration looks correct.')
+  } catch (e) {
+    console.log('Could not validate proper store setup', e.message)
+    process.exit()
+  }
+  if (program.pages) {
+    await migration.migratePages()
+  }
 }
 start()
--- src/migrator.js
+++ src/migrator.js
@@ -1,11 +1,12 @@
 const Shopify = require('shopify-api-node');
 
 class Migrator {
-  constructor(sourceStore, destinationStore) {
+  constructor(sourceStore, destinationStore, verbosity = 4) {
     this.config = {
       source: sourceStore,
       destination: destinationStore
     }
+    this.verbosity = verbosity
     this.source = new Shopify(sourceStore);
     this.destination = new Shopify(destinationStore);
     this.requiredScopes = {
@@ -19,21 +20,92 @@ class Migrator {
       ]
     };
   }
+  info() {
+    if (this.verbosity > 3) {
+      console.info.apply(this, arguments)
+    }
+  }
+  log() {
+    if (this.verbosity > 2) {
+      console.log.apply(this, arguments)
+    }
+  }
+  warn() {
+    if (this.verbosity > 1) {
+      console.warn.apply(this, arguments)
+    }
+  }
+  error() {
+    console.error.apply(this, arguments)
+  }
   async testConnection() {
     const sourceScopes = await this.source.accessScope.list()
     const destinationScopes = await this.destination.accessScope.list()
     this.requiredScopes.source.forEach((scopes) => {
       const scopeFound = !!sourceScopes.find(scope => scopes.indexOf(scope.handle) !== -1)
       if (!scopeFound) {
-        throw new Error(`Source store does not have proper access scope: ${scopes[0]}`)
+        const message = `Source store does not have proper access scope: ${scopes[0]}`
+        this.error(message)
+        throw new Error(message)
       }
     })
     this.requiredScopes.destination.forEach((scopes) => {
       const scopeFound = !!destinationScopes.find(scope => scopes.indexOf(scope.handle) !== -1)
       if (!scopeFound) {
-        throw new Error(`Destination store does not have proper access scope: ${scopes[0]}`)
+        const message = `Destination store does not have proper access scope: ${scopes[0]}`
+        this.error(message)
+        throw new Error(message)
       }
     })
+  }
+  async asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+  }
+  async _getMetafields(resource = null, id = null) {
+    let params = { limit: 250 }
+    if (resource && id) {
+      params.metafield = {
+        owner_resource: resource,
+        owner_id: id
+      }
+    }
+    const metafields = []
+    do {
+      const resourceMetafields = await this.source.metafield.list(params)
+      resourceMetafields.forEach(m => metafields.push(m))
+      params = resourceMetafields.nextPageParameters;
+    } while (params !== undefined);
+    return metafields
+  }
+  async _migratePage(page) {
+    this.info(`[PAGE ${page.id}] ${page.handle} started...`)
+    const metafields = await this._getMetafields('page', page.id)
+    this.info(`[PAGE ${page.id}] has ${metafields.length} metafields...`)
+    const newPage = await this.destination.page.create(page)
+    this.info(`[PAGE ${page.id}] duplicated. New id is ${newPage.id}.`)
+    await this.asyncForEach(metafields, async (metafield) => {
+      delete metafield.id
+      metafield.owner_resource = 'page'
+      metafield.owner_id = newPage.id
+      this.info(`[PAGE ${page.id}] Metafield ${metafield.namespace}.${metafield.key} started`)
+      await this.destination.metafield.create(metafield)
+      this.info(`[PAGE ${page.id}] Metafield ${metafield.namespace}.${metafield.key} done!`)
+    })
+  }
+  async migratePages() {
+    this.log('Page migration started...')
+    let params = { limit: 250 }
+    do {
+      const pages = await this.source.page.list(params)
+      await this.asyncForEach(pages, async (page) => {
+        await this._migratePage(page)
+      })
+      params = pages.nextPageParameters;
+    } while (params !== undefined);
+    this.log('Page migration finished!')
+
   }
 }
 
